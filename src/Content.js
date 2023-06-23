62447a20e7741c4113b1f932643abd69fe88ec82
Pramyagit <prlivetime@gmail.com>
2023-06-23 18:29:41 +0530

    todolist

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './content.css'
 import Itemss from './Itemss';
 // const [name,setName]=useState("subscriber");
